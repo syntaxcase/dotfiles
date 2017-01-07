46d929828d761b63fd75dcebff082031694166ac
Alessandro Carlo Chirico <alessandro.carlo@chiri.co>
2017-01-07 11:35:23 +0100

    External custom file

--- emacs.d-init.el
+++ emacs.d-init.el
@@ -22,6 +22,9 @@
 (add-hook 'minibuffer-setup-hook #'my-minibuffer-setup-hook)
 (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
 
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file :noerror)
+
 ;;; Packages
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
@@ -439,54 +442,7 @@
                           `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
                           `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
 
-;;;;;;;
-;; Automatically generated
-;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "6a925fdf3a7bf2f3901d8fbc4ef64f9b4b4be2c6bed2b0d49d154db0bec91b33" "38ba6a938d67a452aeb1dada9d7cdeca4d9f18114e9fc8ed2b972573138d4664" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "9b94a52c25ea76b72df2050928d18e7fe9060e9c7f7d992f33bf35d4931b0444" "de538b2d1282b23ca41ac5d8b69c033b911521fe27b5e1f59783d2eb20384e1f" default)))
- '(package-selected-packages
-   (quote
-    (org-plus-contrib expand-region god-mode helm-flycheck zenburn-theme yaml-mode which-key web-mode volatile-highlights undo-tree toml-mode tern-auto-complete sx stumpwm-mode spaceline solarized-theme smartparens slime scala-mode2 req-package rainbow-delimiters racer python-mode pp-c-l paredit org-bullets org-beautify-theme org nodejs-repl neotree monokai-theme magit js2-refactor jedi jade-mode indent-guide iedit httprepl helm-themes helm-swoop helm-projectile helm-proc helm-descbinds helm-company helm-cider-history helm-ag haskell-mode groovy-mode goto-chg go-mode geiser fringe-helper flycheck-rust flycheck-pos-tip flycheck-clojure flx fireplace ensime dumb-jump dockerfile-mode docker company-web company-tern company-racer company-jedi company-auctex company-ansible avy anzu anti-zenburn-theme ansible-doc ansible aggressive-indent ag)))
- '(safe-local-variable-values
-   (quote
-    ((Package . DRAKMA)
-     (TeX-master . t)
-     (common-lisp-style . modern)
-     (Base . 10)
-     (Package . HUNCHENTOOT)
-     (Syntax . COMMON-LISP)
-     (Package . CXML)
-     (Encoding . utf-8)
-     (readtable . runes)
-     (Package . SAX)
-     (Syntax . Common-Lisp))))
- '(whitespace-style
-   (quote
-    (face tabs spaces trailing lines space-before-tab indentation empty space-after-tab space-mark tab-mark))))
-
 (provide '.emacs)
-
-;;; .emacs ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.5 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana")))))
 
 (provide 'init)
 ;;; init ends here
