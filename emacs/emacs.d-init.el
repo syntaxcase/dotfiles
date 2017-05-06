785d98f3c27b5b7711a54f9a16930bc3d3846adc
Alessandro Carlo Chirico <alessandro.carlo@chiri.co>
2017-05-07 00:08:05 +0200

    Better js2-mode configuration

--- emacs/emacs.d-init.el
+++ emacs/emacs.d-init.el
@@ -403,7 +403,17 @@
   :interpreter "node"
   :init
   (setq js-indent-level 2)
-  (setq js2-basic-offset 2))
+  (setq js2-basic-offset 2)
+  :config
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  (js2r-add-keybindings-with-prefix "C-c M-r")
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+  (define-key js-mode-map (kbd "M-.") nil)
+
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
 
 (req-package web-mode
   :require flycheck
