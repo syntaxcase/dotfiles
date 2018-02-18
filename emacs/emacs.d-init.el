fc3c91868208d01fca2c5d02d329980766693ee4
Alessandro Carlo Chirico <alessandro.carlo@chiri.co>
2018-02-18 23:27:55 +0100

    Dired-sidebar

--- emacs/emacs.d-init.el
+++ emacs/emacs.d-init.el
@@ -285,6 +285,15 @@
   :init
   (setq magit-completing-read-function 'ivy-completing-read))
 
+(use-package dired-sidebar
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :config
+  (use-package all-the-icons-dired
+    ;; M-x all-the-icons-install-fonts
+    :ensure t
+    :commands (all-the-icons-dired-mode)))
+
 (use-package org
   :mode ("\\.org\\'" . org-mode)
   :bind (("C-c l" . org-store-link)
