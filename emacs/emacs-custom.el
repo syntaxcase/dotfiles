bef284e476d6f18388432e47e7191cdaa3b61b41
Alessandro Carlo Chirico <alessandro.carlo@chiri.co>
2018-06-05 11:25:40 +0200

    Remove org-mode graphic customizations

--- emacs/emacs-custom.el
+++ emacs/emacs-custom.el
@@ -36,21 +36,5 @@
    (quote
     (face tabs spaces trailing lines space-before-tab indentation empty space-after-tab space-mark tab-mark))))
 
-;;; .emacs ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.5 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#839496" :font "Verdana")))))
-
 (provide 'emacs-custom)
 ;;; emacs-custom.el ends here
--- emacs/emacs.d-init.el
+++ emacs/emacs.d-init.el
@@ -642,31 +642,6 @@
       (delete-char -1)
       (insert replacement-char))))
 
-;;; Org-mode customization found at:
-;;; http://www.howardism.org/Technical/Emacs/orgmode-wordprocessor.html
-(font-lock-add-keywords 'org-mode
-                        '(("^ +\\([-*]\\) "
-                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-(let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-                             ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-                             ((x-list-fonts "Verdana")         '(:font "Verdana"))
-                             ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-                             (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-       (base-font-color     (face-foreground 'default nil 'default))
-       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-  (custom-theme-set-faces 'user
-                          `(org-level-8 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-7 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-6 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-5 ((t (,@headline ,@variable-tuple))))
-                          `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-                          `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-                          `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-                          `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-                          `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
-
 (provide '.emacs)
 
 (provide 'init)
