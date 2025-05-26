94a03e2b22999dfc9280575266cbfb91ed1f9b8c
Alessandro Carlo Chirico <alessandro.carlo@chiri.co>
2025-05-25 22:59:35 -0700

    Add the Home Assistant container

--- nixos/minipc/home.nix
+++ nixos/minipc/home.nix
@@ -40,8 +40,26 @@
             addGroups = [ "keep-groups" ];
           };
         };
-      };
-    };
+        home-assistant = {
+          autoStart = true;
+          serviceConfig = {
+            Restart = "always";
+          };
+          containerConfig = {
+            image = "ghcr.io/home-assistant/home-assistant:stable";
+            publishPorts = [ "0.0.0.0:8123:8123" ];
+            userns = "keep-id";
+            networks = [ "host" ];
+            devices = [ "/dev/ttyZBT1:/dev/ttyZBT1" ];
+            volumes = [ "/home/ha/ha/config:/config" "/run/dbus:/run/dbus:ro" ];
+            addGroups = [ "keep-groups" ];
+            environments = {
+              TZ = "America/Los_Angeles";
+            };
+          };
+        };
+       };
+     };
 
     # The state version is required and should stay at the version you *originally* installed.
     home.stateVersion = "24.11";
