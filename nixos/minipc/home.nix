3fc8a17bb8aee656ce5f93fa6be78effad9f45c4
Alessandro Carlo Chirico <alessandro.carlo@chiri.co>
2025-06-01 19:24:57 -0700

    Add nvd

--- nixos/minipc/home.nix
+++ nixos/minipc/home.nix
@@ -7,7 +7,7 @@
   home-manager.useUserPackages = true;
 
   home-manager.users.acc = { pkgs, ... }: {
-    home.packages = [ pkgs.git ];
+    home.packages = [ pkgs.git pkgs.nvd ];
     programs.bash.enable = true;
 
     # The state version is required and should stay at the version you *originally* installed.
